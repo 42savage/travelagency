bec73b94337165c70809dac73688f4ae22be16f9
42savage <shizz0@outlook.com>
2021-04-06 13:07:47 +0200

    Initial commit

---
+++ components/carousel.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="carousel-wrapper">
+    <div
+      class="single-slide"
+      v-for="slide in slides"
+      :key="slide.id"
+      :style="{
+        backgroundImage:
+          'url(' + require('~/assets/' + slide.image + '.jpg') + ')',
+      }"
+    >
+      <h3>{{ slide.title }}</h3>
+      <p>{{ slide.subTitle }}</p>
+      <p>{{ slide.text }}</p>
+      <button class="button">explore</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slides: [
+        {
+          id: 0,
+          title: 'Greece',
+          subTitle: 'Babaluba cape',
+          text: 'One of the best places in europe',
+          image: 'background',
+        },
+        {
+          id: 1,
+          title: 'Rome',
+          subTitle: 'The Colloseum',
+          text: 'An oval amphitheatre in the centre of the city of Rome',
+          image: 'background2',
+        },
+        {
+          id: 2,
+          title: 'Rejkiawik',
+          subTitle: 'Visit The Iceland',
+          text: 'Nordic island country in the North Atlantic Ocean',
+          image: 'background3',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.single-slide {
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+}
+</style>
---
+++ components/navigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="nav_wrapper">
+    <nav>
+      <div class="logo">
+        <h3><b>Generic</b>Travel</h3>
+        <p>agency</p>
+      </div>
+      <button>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
+
+<style scoped lang="scss">
+.nav_wrapper {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 5;
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 18px;
+  h3 {
+    font-size: 24px;
+  }
+  h3,
+  p {
+    color: white;
+  }
+  p {
+    text-align: right;
+    line-height: 5px;
+    font-size: 18px;
+  }
+  button {
+    background: none;
+    border: none;
+    div {
+      background: white;
+      width: 36px;
+      height: 3px;
+      margin: 6px 0;
+      &:nth-child(1) {
+        width: 30px;
+      }
+    }
+  }
+}
+</style>
